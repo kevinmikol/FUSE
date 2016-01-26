b8fd945fc131423fafd1f43c2f5722b7d543ece9
Evan <me@kevinmikol.com>
2016-01-26 16:38:55 -0600

    font awesome

--- includes/header.php
+++ includes/header.php
@@ -15,6 +15,8 @@
     <!-- Bootstrap Core CSS -->
     <link href="assets/css/bootstrap.min.css" rel="stylesheet">
     <link href="assets/css/style.css" rel="stylesheet">
+    
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
