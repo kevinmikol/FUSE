d59d15c2d5e260b2ee51e1ee40d0c92875a7323b
Kevin Mikolajczak <me@kevinmikol.com>
2016-01-29 12:07:12 -0600

    Delete config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-    //Database Information
-    $dbhost = "trinix.co";
-    $dbname = "fuse_cms";
-    $dbuser = "fuse_cms";
-    $dbpass = "3ZM3TI8q3t";
-
-    $dbh = new PDO('mysql:host='.$dbhost.';dbname='.$dbname.';charset=utf8', $dbuser, $dbpass);
-?>
